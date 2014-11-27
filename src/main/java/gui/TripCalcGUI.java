1fb47be48d436a007db0407e095940dec12a7862
kincoc11 <kincoc11@htlkaindorf.at>
2014-11-27 12:20:03 +0100

    GUI NEW - händisch

--- src/main/java/gui/TripCalcGUI.java
+++ src/main/java/gui/TripCalcGUI.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,8 +13,8 @@ public class TripCalcGUI extends JFrame{
 
 
 
-    private JPanel paLeft;
-    private JPanel paRight;
+    private JPanel paLeft = new JPanel();
+    private JPanel paRight = new JPanel();
 
     private JButton btSubmitT;
     private JTextField txOutputT;
@@ -27,18 +28,17 @@ public class TripCalcGUI extends JFrame{
     private JLabel lbCargoT;
     private JLabel lbAxlesT;
     private JLabel lbAdBlue;
-
-    private JButton btSubmitCar;
-    private JTextField txOutputCar;
-    private JPanel paTop;
-    private JPanel paMiddle;
-    private JPanel paBottom;
-    private JTextField txFuelConsumptionCar;
-    private JTextField txCargoCar;
-    private JComboBox cbTypeOfFuelCar;
-    private JLabel lbFuelConsumptionCar;
-    private JLabel lbTypeOfFuelCar;
-    private JLabel lbCargoCar;
+    private JButton btSubmitCar = new JButton("Submit");
+    private JTextField txOutputCar = new JTextField();
+    private JPanel paTop = new JPanel();
+    private JPanel paMiddle = new JPanel();
+    private JPanel paBottom = new JPanel();
+    private JTextField txFuelConsumptionCar = new JTextField();
+    private JTextField txCargoCar = new JTextField();
+    private JComboBox cbTypeOfFuelCar = new JComboBox();
+    private JLabel lbFuelConsumptionCar = new JLabel("Fuel consumption: ");
+    private JLabel lbTypeOfFuelCar = new JLabel("Type of fuel: ");
+    private JLabel lbCargoCar = new JLabel("Cargo in kg: ");
 
 
     public TripCalcGUI() throws HeadlessException
@@ -52,9 +52,25 @@ public class TripCalcGUI extends JFrame{
     public void init()
     {
         Container con = this.getContentPane();
-        con.setLayout(new GridLayout(2,1));
+        con.setLayout(new GridLayout(1,2));
 
-
+        con.add(paLeft);
+        paLeft.setLayout(new GridLayout(5, 1));
+        paLeft.setBorder(new TitledBorder("Car"));
+        paLeft.add(paTop);
+        paLeft.add(paMiddle);
+        paLeft.add(paBottom);
+        paLeft.add(btSubmitCar);
+        paLeft.add(txOutputCar);
+        paTop.setLayout(new GridLayout(1,2));
+        paMiddle.setLayout(new GridLayout(1,2));
+        paBottom.setLayout(new GridLayout(1,2));
+        paTop.add(lbFuelConsumptionCar);
+        paTop.add(txFuelConsumptionCar);
+        paMiddle.add(lbTypeOfFuelCar);
+        paMiddle.add(cbTypeOfFuelCar);
+        paBottom.add(lbCargoCar);
+        paBottom.add(txCargoCar);
 
     }
 
