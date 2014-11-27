a70b354317ba788416318092315fbef8d70a0f48
haryvc11 <haryvc11@htlkaindorf.at>
2014-11-27 11:22:59 +0100

    GUI

--- src/main/java/gui/TripCalculatorGUI.java
+++ src/main/java/gui/TripCalculatorGUI.java
@@ -1,7 +1,11 @@
 package gui;
 
+import javax.swing.*;
+
 /**
  * Created by Corinna on 27.11.2014.
  */
 public class TripCalculatorGUI {
+    private JPanel paLeft;
+    private JPanel paRight;
 }
