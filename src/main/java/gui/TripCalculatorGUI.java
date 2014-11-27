c1a8973ea1d78cc912e6abdcbfdec711e1fe99f6
kincoc11 <kincoc11@htlkaindorf.at>
2014-11-27 11:37:26 +0100

    GUI - Left Panel designed

--- src/main/java/gui/TripCalculatorGUI.java
+++ src/main/java/gui/TripCalculatorGUI.java
@@ -8,4 +8,15 @@ import javax.swing.*;
 public class TripCalculatorGUI {
     private JPanel paLeft;
     private JPanel paRight;
+    private JButton btSubmitCar;
+    private JTextField txOutputCar;
+    private JPanel paTop;
+    private JPanel paMiddle;
+    private JPanel paBottom;
+    private JTextField txFuelConsumptionCar;
+    private JTextField txCargoCar;
+    private JComboBox cbTypeOfFuelCar;
+    private JLabel lbFuelConsumptionCar;
+    private JLabel lbTypeOfFuelCar;
+    private JLabel lbCargoCar;
 }
