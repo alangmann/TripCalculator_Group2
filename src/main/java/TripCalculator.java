49294d59e083106b3caef76dc8e5b2ca7a50793b
haryvc11 <haryvc11@htlkaindorf.at>
2014-11-27 10:51:54 +0100

    asd

--- src/main/java/TripCalculator.java
+++ src/main/java/TripCalculator.java
@@ -1,10 +1,12 @@
 /**
  * Created by andreas.langmann on 12.11.2014.
  */
-public class TripCalculator {
+public class TripCalculator
+{
 
-    // TODO: implement me :)
-    //Hallooooooo
-    //HI
-    //abcsadasd
+// TODO: implement me :)
+//Hallooooooo
+//HI
+//abcsadasd
+//sadkjksdfh
 }
