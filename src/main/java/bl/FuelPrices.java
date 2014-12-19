0f76dee2cc05caa581c36fdc0983d6cf7e25b257
kincoc11 <kincoc11@htlkaindorf.at>
2014-12-19 13:34:36 +0100

    datenklasse für FuelPrices geaddet

---
+++ src/main/java/bl/FuelPrices.java
@@ -0,0 +1,57 @@
+package bl;
+
+/**
+ * Created by Corinna on 19.12.2014.
+ */
+
+/*
+We added this class because we didn't know how to accomplish the task
+with the different prices of fuel per weekday
+without this class
+ */
+public class FuelPrices
+{
+    private String weekDay;
+    private double dieselPrice;
+    private double patrolPrice;
+
+    public FuelPrices(String weekDay, double dieselPrice, double patrolPrice) {
+        this.weekDay = weekDay;
+        this.dieselPrice = dieselPrice;
+        this.patrolPrice = patrolPrice;
+    }
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(String weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public double getDieselPrice() {
+        return dieselPrice;
+    }
+
+    public void setDieselPrice(double dieselPrice) {
+        this.dieselPrice = dieselPrice;
+    }
+
+    public double getPatrolPrice() {
+        return patrolPrice;
+    }
+
+    public void setPatrolPrice(double patrolPrice) {
+        this.patrolPrice = patrolPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "FuelPrices{" +
+                "weekDay='" + weekDay + '\'' +
+                ", dieselPrice=" + dieselPrice +
+                ", patrolPrice=" + patrolPrice +
+                '}';
+    }
+}
+
