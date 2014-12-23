a020ec54ce591bcea48c415b265eec2f4f49a147
kincoc11 <kincoc11@htlkaindorf.at>
2014-12-23 10:45:30 +0100

    richtige Formate bei der Ausgabe

--- src/test/java/TripCalculatorTest.java
+++ src/test/java/TripCalculatorTest.java
@@ -3,7 +3,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class TripCalculatorTest {
+public class TripCalculatorTest
+{
 
     @Test
     public void testDummy() {
